b129700e2ebda600d3c6291cdcf8958d74b5f88e
Milki Legesse <nitfanono@gmail.com>
2025-09-07 21:00:06 +0300

    finished with the ui

--- app/component/contact-us.tsx
+++ app/component/contact-us.tsx
@@ -12,37 +12,58 @@ export default function ContactUsForm() {
 
       <form className="p-4 flex flex-col gap-y-3">
         <div className="flex flex-col md:flex-row gap-4">
-          <input
-            type="text"
-            className="w-full px-4 py-2 border-1 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            placeholder="First Name"
-            required
-          />
-          <input
-            type="text"
-            className="w-full px-4 py-2 border-1 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            placeholder="Last Name"
-            required
-          />
+          <div className="w-full flex flex-col ">
+            <label htmlFor="firstname" className="mb-1">
+              First Name <span className="text-red-600">*</span>
+            </label>
+            <input
+              type="text"
+              id="firstname"
+              className="w-full px-4 py-2 border-1 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="First Name"
+              required
+            />
+          </div>
+          <div className="w-full flex flex-col ">
+            <label htmlFor="lastname" className="mb-1">
+              Last Name <span className="text-red-600">*</span>
+            </label>
+            <input
+              type="text"
+              id="lastname"
+              className="w-full px-4 py-2 border-1 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Last Name"
+              required
+            />
+          </div>
         </div>
-        <div className="w-full">
+        <div className="w-full flex flex-col">
+          <label htmlFor="email" className="mb-1">
+            Email <span className="text-red-600">*</span>
+          </label>
           <input
             type="email"
+            id="email"
             className="w-full px-4 py-2 border-1 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
             placeholder="Email"
             required
           />
         </div>
-        <div className="w-full flex">
-          <span className="bg-gray-100 text-gray-800 px-3 flex items-center rounded-md">
-            +251
-          </span>
-          <input
-            type="tel"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            placeholder="Phone Number"
-            required
-          />
+        <div className="flex flex-col">
+          <label htmlFor="phone" className="mb-1">
+            Phone Number <span className="text-red-600">*</span>
+          </label>
+          <div className="w-full flex">
+            <span className="bg-gray-100 text-gray-800 px-3 flex items-center rounded-md">
+              +251
+            </span>
+            <input
+              type="tel"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Phone Number"
+              required
+            />
+          </div>
         </div>
         <div className="w-full flex">
           <textarea
