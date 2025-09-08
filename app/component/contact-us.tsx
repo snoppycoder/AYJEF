73337bbea528312f312ab2e064c77cc5c51768df
Milki Legesse <nitfanono@gmail.com>
2025-09-08 17:49:32 +0300

    last touch

--- app/component/contact-us.tsx
+++ app/component/contact-us.tsx
@@ -59,6 +59,7 @@ export default function ContactUsForm() {
             </span>
             <input
               type="tel"
+              id="phone"
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="Phone Number"
               required
